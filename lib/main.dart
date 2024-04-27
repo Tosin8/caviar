f9288f506112fdbc1d7ad491fa4f9faf3b702cdf
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-27 19:56:36 +0100

    updating with bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
+import 'screens/splash.dart';
+import 'shop/home.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -27,7 +30,7 @@ class MyApp extends StatelessWidget {
           iconTheme: const IconThemeData(color: kDark), 
           primarySwatch: Colors.grey, 
         ),
-        home:  const SplashScreen(), 
+        home:  const Home(), 
       ); 
       }, 
       
@@ -36,11 +39,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
---
+++ lib/shop/home.dart
@@ -0,0 +1,27 @@
+import 'package:caviar/util/constants/const.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      body: Stack( 
+        children: [
+          Container(
+            height: height, 
+            width: width, 
+            color: kOffWhite, 
+          ), 
+          Align(
+            alignment: Alignment.bottomCenter, 
+            child: Theme(data: Theme.of(context).copyWith(canvasColor: kPrimary),
+             child: BottomNavigationBar(
+              items: []))
+          )
+        ]
+      )
+    );
+  }
+}
