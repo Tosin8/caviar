67e8ec0322e3e6b15b53dcf8e7c6564574cc0275
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-16 19:16:23 +0100

    Update home.dart

--- lib/shop/home.dart
+++ lib/shop/home.dart
@@ -11,37 +11,37 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final controller = Get.put(TabIndexController());
-    return Obx(() => Scaffold( 
-          body: Stack( 
-            children: [
-              Container(
-                height: height, 
-                width: width, 
-                color: kOffWhite, 
-              ), 
-              Align(
-                alignment: Alignment.bottomCenter, 
-                child: Theme(data: Theme.of(context).copyWith(canvasColor: kPrimary),showSelectedLabels: false, 
+    return Scaffold( 
+      body: Stack( 
+        children: [
+          Container(
+            height: height, 
+            width: width, 
+            color: kOffWhite, 
+          ), 
+          Align(
+            alignment: Alignment.bottomCenter, 
+            child: Theme(data: Theme.of(context).copyWith(canvasColor: kPrimary),
+             child: BottomNavigationBar(
+              showSelectedLabels: false, 
               showUnselectedLabels: false , 
               unselectedIconTheme: const IconThemeData(color: Colors.black38),
               selectedIconTheme: const IconThemeData(color: kSecondary),
               onTap: (value) {
                 print(value); 
               },
-              items: const [
-                BottomNavigationBarItem(
+              items: [
+                const BottomNavigationBarItem(
                   icon: Icon(Icons.home), 
                 label: 'Home'), 
-                BottomNavigationBarItem(
+                const BottomNavigationBarItem(
                   icon: Icon(Icons.search), 
-                label: 'Home'), BottomNavigationBarItem(
+                label: 'Home'), const BottomNavigationBarItem(
                   icon: Icon(Icons.shopping_cart), 
-                label: 'Home'), BottomNavigationBarItem(
+                label: 'Home'), const BottomNavigationBarItem(
                   icon: Icon(Icons.person), 
                 label: 'Home'), 
-              ],
-                 child: BottomNavigationBar(
-        )))
+              ]))
           )
         ]
       )
