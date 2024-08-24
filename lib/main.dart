537a2b631773782ae9f366d0c93d0c5a47304bf5
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-24 22:16:04 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:caviar/util/constants/const.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:get/get.dart';
 
 import 'screens/splash.dart';
